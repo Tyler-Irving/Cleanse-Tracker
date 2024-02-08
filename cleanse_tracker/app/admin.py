2faa4335df82139cd69520427ecfdc5b525dd2d8
Tyler-Irving <tirving2001@gmail.com>
2024-02-08 13:46:09 -0600

    registering models for admin page

--- cleanse_tracker/app/admin.py
+++ cleanse_tracker/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cleanse, CleanseEntry, Restriction
 
 # Register your models here.
+admin.site.register(Cleanse)
+
+admin.site.register(CleanseEntry)
+
+admin.site.register(Restriction)
