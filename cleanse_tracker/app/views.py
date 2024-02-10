7ce95e88118ec7c969c10fc4f9809fa40e78fa9a
Tyler-Irving <tirving2001@gmail.com>
2024-02-10 13:41:41 -0600

    landing page view and get method for user creation

--- cleanse_tracker/app/views.py
+++ cleanse_tracker/app/views.py
@@ -1,6 +1,8 @@
-from rest_framework import generics
+from rest_framework.generics import CreateAPIView, DestroyAPIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from .serializers import (
     UserSerializer,
@@ -9,20 +11,29 @@ from .serializers import (
     RestrictionSerializer,
 )
 from .models import Cleanse, Restriction, CleanseEntry
+from .forms import UserCreationForm
 
 
-class CreateUserAndDeleteView(generics.CreateAPIView, generics.DestroyAPIView):
+class LandingPageView(APIView):
+    def get(self, request, *args, **kwargs):
+        return render(request, "landing_page.html")
+
+
+class CreateUserAndDeleteView(CreateAPIView, DestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+    def get(self, request, *args, **kwargs):
+        form = UserCreationForm()
+        return render(request, "create_user_form.html", {"form": form})
 
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
         headers = self.get_success_headers(serializer.data)
-        return Response(
-            serializer.data, status=status.HTTP_201_CREATED, headers=headers
-        )
+        Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return redirect("home")
 
     def delete(self, request, *args, **kwargs):
         user = self.get_object()
@@ -30,7 +41,7 @@ class CreateUserAndDeleteView(generics.CreateAPIView, generics.DestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CleanseCreateAndDeleteView(generics.CreateAPIView, generics.DestroyAPIView):
+class CleanseCreateAndDeleteView(CreateAPIView, DestroyAPIView):
     queryset = Cleanse.objects.all()
     serializer_class = CleanseSerializer
 
@@ -49,7 +60,7 @@ class CleanseCreateAndDeleteView(generics.CreateAPIView, generics.DestroyAPIView
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class RestrictionCreateAndDeleteView(generics.CreateAPIView, generics.DestroyAPIView):
+class RestrictionCreateAndDeleteView(CreateAPIView, DestroyAPIView):
     queryset = Restriction.objects.all()
     serializer_class = RestrictionSerializer
 
@@ -68,7 +79,7 @@ class RestrictionCreateAndDeleteView(generics.CreateAPIView, generics.DestroyAPI
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CleanseCreateView(generics.CreateAPIView):
+class CleanseCreateView(CreateAPIView):
     queryset = CleanseEntry.objects.all()
     serializer_class = CleanseEntrySerializer
 
