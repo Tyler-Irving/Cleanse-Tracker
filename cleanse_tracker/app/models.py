1ead0edd26be50a3053af514ed1fcb1682a10531
Tyler-Irving <tirving2001@gmail.com>
2024-02-10 15:06:14 -0600

    black formatter

--- cleanse_tracker/app/models.py
+++ cleanse_tracker/app/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Cleanse(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     start_date = models.DateField()
     end_date = models.DateField()
 
+
 class Restriction(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
 
 class CleanseEntry(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
