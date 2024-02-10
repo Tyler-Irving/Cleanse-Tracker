680e03a315197e2e1e8a4817840f2f86d12cfdb4
Tyler-Irving <tirving2001@gmail.com>
2024-02-10 15:30:52 -0600

    added __str__ for the admin page

--- cleanse_tracker/app/models.py
+++ cleanse_tracker/app/models.py
@@ -8,10 +8,16 @@ class Cleanse(models.Model):
     start_date = models.DateField()
     end_date = models.DateField()
 
+    def __str__(self):
+        return self.name
+
 
 class Restriction(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class CleanseEntry(models.Model):
@@ -19,3 +25,6 @@ class CleanseEntry(models.Model):
     cleanse = models.ForeignKey(Cleanse, on_delete=models.CASCADE)
     restriction = models.ForeignKey(Restriction, on_delete=models.CASCADE)
     is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user} - {self.cleanse}"
